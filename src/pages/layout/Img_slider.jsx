d36c07eb147bbd0774ca001df29c4e70d2385780
italeenamk <italeenamk@gmail.com>
2023-04-05 14:25:18 +0500

    sidebar-complete

--- src/pages/layout/Img_slider.jsx
+++ src/pages/layout/Img_slider.jsx
@@ -33,10 +33,28 @@ const Img_slider = () => {
                                       <div className="overlay">
                                           <div className="hover-effect-content">
                                               <div className="hover-effect-heading">
-                                                  <h3>Project Number 3</h3>
+                                                  <h3>Project Number 4</h3>
                                                   <p>web, branding</p>
                                               </div>
                                               <div className="hover-effect-button" style={{marginTop:"-306px"}}>
+                                                  <a href=""> <i className="fa-solid fa-link"></i> </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="tile">
+                              <div className="img-sidebar-content">
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_9.jpg.webp" className="img-fluid"/>
+                                  <div className="portfolio-img-hover-content">
+                                      <div className="overlay">
+                                          <div className="hover-effect-content">
+                                              <div className="hover-effect-heading">
+                                                  <h3>Project Number 7</h3>
+                                                  <p>web, branding</p>
+                                              </div>
+                                              <div className="hover-effect-button">
                                                   <a href=""> <i className="fa-solid fa-link"></i> </a>
                                               </div>
                                           </div>
@@ -71,10 +89,46 @@ const Img_slider = () => {
                                       <div className="overlay">
                                           <div className="hover-effect-content">
                                               <div className="hover-effect-heading">
-                                                  <h3>Project Number 4</h3>
+                                                  <h3>Project Number 5</h3>
                                                   <p>web, branding</p>
                                               </div>
                                               <div className="hover-effect-button" style={{marginTop:"-410px"}}>
+                                                  <a href=""> <i className="fa-solid fa-link"></i> </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="tile" style={{height:"380px"}}>
+                              <div className="img-sidebar-content">
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_8.jpg.webp" className="img-fluid"/>
+                                  <div className="portfolio-img-hover-content" style={{top:"304px"}}>
+                                      <div className="overlay">
+                                          <div className="hover-effect-content">
+                                              <div className="hover-effect-heading">
+                                                  <h3>Project Number 8</h3>
+                                                  <p>web, branding</p>
+                                              </div>
+                                              <div className="hover-effect-button" style={{marginTop:"-306px"}}>
+                                                  <a href=""> <i className="fa-solid fa-link"></i> </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="tile" style={{height:"255px"}}>
+                              <div className="img-sidebar-content">
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_11.jpg.webp" className="img-fluid"/>
+                                  <div className="portfolio-img-hover-content" style={{top:"179px"}}>
+                                      <div className="overlay">
+                                          <div className="hover-effect-content">
+                                              <div className="hover-effect-heading">
+                                                  <h3>Project Number 10</h3>
+                                                  <p>web, branding</p>
+                                              </div>
+                                              <div className="hover-effect-button" style={{marginTop:"-183px"}}>
                                                   <a href=""> <i className="fa-solid fa-link"></i> </a>
                                               </div>
                                           </div>
@@ -102,17 +156,71 @@ const Img_slider = () => {
                                   </div>
                               </div>
                           </div>
-                          <div className="tile" style={{height:"380px"}}>
+                          <div className="tile" style={{height:"255px"}}>
                               <div className="img-sidebar-content">
-                                  <img src="https://preview.colorlib.com/theme/space/images/img_2.jpg.webp" className="img-fluid"/>
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_5.jpg.webp" className="img-fluid"/>
+                                  <div className="portfolio-img-hover-content" style={{top:"179px"}}>
+                                      <div className="overlay">
+                                          <div className="hover-effect-content">
+                                              <div className="hover-effect-heading">
+                                                  <h3>Project Number 6</h3>
+                                                  <p>web, branding</p>
+                                              </div>
+                                              <div className="hover-effect-button" style={{marginTop:"-183px"}}>
+                                                  <a href=""> <i className="fa-solid fa-link"></i> </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="tile" style={{height:"389px"}}>
+                              <div className="img-sidebar-content">
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_7.jpg.webp" className="img-fluid"/>
                                   <div className="portfolio-img-hover-content" style={{top:"304px"}}>
                                       <div className="overlay">
                                           <div className="hover-effect-content">
                                               <div className="hover-effect-heading">
-                                                  <h3>Project Number 2</h3>
+                                                  <h3>Project Number 9</h3>
                                                   <p>web, branding</p>
                                               </div>
                                               <div className="hover-effect-button" style={{marginTop:"-306px"}}>
+                                                  <a href=""> <i className="fa-solid fa-link"></i> </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="tile" style={{height:"223px"}}>
+                              <div className="img-sidebar-content">
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_10.jpg.webp" className="img-fluid"/>
+                                  <div className="portfolio-img-hover-content" style={{top:"146px"}}>
+                                      <div className="overlay">
+                                          <div className="hover-effect-content">
+                                              <div className="hover-effect-heading">
+                                                  <h3>Project Number 11</h3>
+                                                  <p>web, branding</p>
+                                              </div>
+                                              <div className="hover-effect-button" style={{marginTop:"-150px"}}>
+                                                  <a href=""> <i className="fa-solid fa-link"></i> </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="tile" style={{height:"223px"}}>
+                              <div className="img-sidebar-content">
+                                  <img src="https://preview.colorlib.com/theme/space/images/img_12.jpg.webp" className="img-fluid"/>
+                                  <div className="portfolio-img-hover-content" style={{top:"146px"}}>
+                                      <div className="overlay">
+                                          <div className="hover-effect-content">
+                                              <div className="hover-effect-heading">
+                                                  <h3>Project Number 12</h3>
+                                                  <p>web, branding</p>
+                                              </div>
+                                              <div className="hover-effect-button" style={{marginTop:"-150px"}}>
                                                   <a href=""> <i className="fa-solid fa-link"></i> </a>
                                               </div>
                                           </div>
