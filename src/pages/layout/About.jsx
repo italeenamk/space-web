70139b5e9cc00e4dbd5f56a6b915ae2800b02370
italeenamk <italeenamk@gmail.com>
2023-04-06 19:48:34 +0500

    working...

--- src/pages/layout/About.jsx
+++ src/pages/layout/About.jsx
@@ -31,11 +31,32 @@ const About = () => {
                               </div>
                               <div className="experience-main-content">
                                   <span>12 Years of experience</span>
-                                  <a href=""> See portfolio</a>
-                                  <a href="">  Download my CV</a>
+                                  <a href=""><i className="fa-solid fa-arrow-right"></i> See portfolio</a>
+                                  <a href=""><i className="fa-solid fa-arrow-right"></i>  Download my CV</a>
                               </div>
                           </div>
-                          <div className="col-md-6 col-sm-12"></div>
+                          <div className="col-md-6 col-sm-12">
+                              <div className="about-me-information-content">
+                                  <div className="bio-content">
+                                      <h3>Bio</h3>
+                                      <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+                                          Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                          It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                                  </div>
+                                  <div className="work-experience-main-content mt-5">
+                                      <div className="bio-content">
+                                          <h3>Work Experience</h3>
+                                      </div>
+                                      <div className="row">
+                                          <div className="col-md-6 col-sm-12">
+                                              <div className="experience-content"></div>
+                                          </div>
+                                          <div className="col-md-6 col-sm-12"></div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
                       </div>
                   </div>
               </div>
