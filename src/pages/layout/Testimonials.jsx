a671a60ed3c025e62ca3a5616ece893b710c7212
italeenamk <italeenamk@gmail.com>
2023-04-07 22:00:36 +0500

    working...

--- src/pages/layout/Testimonials.jsx
+++ src/pages/layout/Testimonials.jsx
@@ -1,3 +1,4 @@
+import '../style/testimonial.css';
 
 
 const Testimonials = () => {
@@ -13,6 +14,51 @@ const Testimonials = () => {
                                   <p>What clients say...</p>
                                   <div className="waves-content">
                                       <i className="fa-solid fa-wave-square"></i><i className="fa-solid fa-wave-square"></i><i className="fa-solid fa-wave-square"></i>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="testimonial-card-main-content">
+                  <div className="container-fluid">
+                      <div className="row">
+                          <div className="col-md-4 col-sm-12 text-center">
+                              <div className="testimonial-card">
+                                  <div className="card">
+                                      <div className="testimonial-information-content">
+                                          <img src="https://preview.colorlib.com/theme/space/images/person_3.jpg.webp" alt=""/>
+                                          <h3>James Smith</h3>
+                                          <span>CEO & Co-Founder</span>
+                                          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+                                              blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="col-md-4 col-sm-12 text-center">
+                              <div className="testimonial-card">
+                                  <div className="card">
+                                      <div className="testimonial-information-content">
+                                          <img src="https://preview.colorlib.com/theme/space/images/person_4.jpg.webp" alt=""/>
+                                          <h3>James Smith</h3>
+                                          <span>CEO & Co-Founder</span>
+                                          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+                                              blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="col-md-4 col-sm-12 text-center">
+                              <div className="testimonial-card">
+                                  <div className="card">
+                                      <div className="testimonial-information-content">
+                                          <img src="https://preview.colorlib.com/theme/space/images/person_1.jpg.webp" alt=""/>
+                                          <h3>James Smith</h3>
+                                          <span>CEO & Co-Founder</span>
+                                          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+                                              blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                                      </div>
                                   </div>
                               </div>
                           </div>
